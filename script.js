807b4ef2e3c11c6c7cc94c16f641376411b8e778
Samuel Alves <samueltheodoro21@gmail.com>
2023-03-12 19:44:00 -0300

    responsive adjust

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 const globalFunction = () => {
   const logo = document.querySelector('.logo')
+  const sections = document.querySelectorAll('.sections')
+  const section02 = document.querySelector('.section02')
+
+  sections.forEach((section) => {
+    section.style.height = `${window.innerHeight}px`
+    console.log(section)
+  })
+
+  if (document.documentElement.clientWidth < 768) {
+    section02.style.height = `${(40 / 100) * window.innerHeight}px`
+  } else {
+    section02.style.height = `${window.innerHeight}px`
+  }
 
   const handleResize = () => {
     if (document.documentElement.clientWidth < 768) {
       logo.src = 'img/section01-logo-mobile.png'
+      section02.style.height = `${(40 / 100) * window.innerHeight}px`
     } else {
       logo.src = 'img/section01-logo.png'
+      section02.style.height = `${window.innerHeight}px`
     }
   }
 
@@ -13,3 +28,5 @@ const globalFunction = () => {
 }
 
 globalFunction()
+
+// 40%
